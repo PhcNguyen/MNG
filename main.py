d136b620f1aef7230aa6fb2b6c450d727e757ada
PHÚC NGUYÊN <138844322+PhcNguyen@users.noreply.github.com>
2023-11-19 19:18:47 +0700

    Update 2.0

--- main.py
+++ main.py
@@ -1,60 +1,37 @@
+# [IMPORT]
 import telebot
-from keep_alive import keep_alive
-from time import sleep
-from docs import UpdateSheet, GetData, DeleteData, SumData
+from KEY import API_KEY
+from threading import Thread
+from telebot import types
+from CoreSystem import UpdataSheet, DeleteSheet, SeeList
 
-keep_alive()
-#keep_alive để bot 24/24 khi bạn sài replit
-#sử dụng thêm UpTimeRobot nha !
-#Chạy vps thì xoá file keep_alive()
-API_KEY = "key bot telegram cua ban"
+# [CODE]
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
+print("[SYSTEM] Start Bot")
 
-@bot.message_handler(commands=['help'])
-def start(msg):
-  bot.send_message(msg.chat.id, '/s - Thêm Dữ Liệu.\n/g - Hiển Thị Dữ Liệu.\n/sum - Tính Tổng Dữ Liệu.')
-  
-@bot.message_handler(commands=["s"])
-def update(msg):
-  msgChat = "Bạn đã nhập sai Cú Pháp !\n  /s <Tên> <Giá>"
-  data = msg.text
-  CompareData = msg.text
-  CompareData = CompareData.split()
-  MC = len(CompareData) - 1
-  try:
-    Money = int(CompareData[MC])
-    print(Money)
-    text=bot.send_message(msg.chat.id, ".")
-    bot.edit_message_text("..", chat_id=msg.chat.id, message_id=text.message_id)
-    bot.edit_message_text("...", chat_id=msg.chat.id, message_id=text.message_id)
-    UpdateSheet(data)
-    bot.edit_message_text("Done !", chat_id=msg.chat.id, message_id=text.message_id)
-  except:
-    id=bot.send_message(msg.chat.id, msgChat)
-    sleep(5)
-    bot.delete_message(msg.chat.id, id.message_id)
-    
-@bot.message_handler(commands=["g"])
-def getData(msg):
-  MsgID2 = GetData()
-  print(MsgID2)
-  for I in range(len(MsgID2)):
-    bot.send_message(msg.chat.id, MsgID2[I])
-    
-@bot.message_handler(commands=["sum"])
-def SumMoney(msg):
-  Sum = SumData()
-  bot.send_message(msg.chat.id, f"Tồng: {Sum:,} !")
-  
-@bot.message_handler(commands=["delete"])
+def CmdSever(msg):
+	user = msg.from_user.id
+	text = msg.text
+	print(f"\nUser: {user}\nChat: {text}")
+	
+@bot.callback_query_handler(func=lambda callback:True)
+def callback_inline(callback):
+	bot.delete_message(callback.message.chat.id, callback.message.message_id)
+	
+@bot.message_handler(commands=['a'])
+def Update(msg):
+	CmdSever(msg)
+	UpdataSheet(msg)
+	
+@bot.message_handler(commands=['del'])
 def Delete(msg):
-  data = msg.text
-  data = data.split()
-  try:
-    ID=bot.send_message(msg.chat.id, "...")
-    DeleteData(data[-1])
-    bot.edit_message_text("Done !", chat_id=msg.chat.id, message_id=ID.message_id)
-  except:
-    bot.send_message(msg.chat.id, "Bạn đã nhập sai Cú Pháp!\n/delete <ID>")
-    
-bot.polling()
+	CmdSever(msg)
+	DeleteSheet(msg)
+
+@bot.message_handler(commands=['list'])
+def List(msg):
+	CmdSever(msg)
+	SeeList(msg)
+
+if (__name__ == "__main__"):
+	Thread(target=callback_inline, args=(bot.infinity_polling())).start()
