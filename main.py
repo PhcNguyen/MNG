764a53964cbce75768c18630ba3257d1c96440b6
PHÚC NGUYÊN <138844322+PhcNguyen@users.noreply.github.com>
2023-12-02 10:24:20 +0700

    Update 0.6

--- main.py
+++ main.py
@@ -11,28 +11,20 @@ bot = telebot.TeleBot(API_KEY, parse_mode=None)
 keep_alive()
 print("[SYSTEM] Start Bot")
 
-def CmdSever(msg):
-	user = msg.from_user.id
-	text = msg.text
-	print(f"\nUser: {user}\nChat: {text}")
-	
 @bot.callback_query_handler(func=lambda callback:True)
 def callback_inline(callback):
 	bot.delete_message(callback.message.chat.id, callback.message.message_id)
 	
 @bot.message_handler(commands=['a'])
 def Update(msg):
-	CmdSever(msg)
 	UpdataSheet(msg)
 	
 @bot.message_handler(commands=['del'])
 def Delete(msg):
-	CmdSever(msg)
 	DeleteSheet(msg)
 
 @bot.message_handler(commands=['list'])
 def List(msg):
-	CmdSever(msg)
 	SeeList(msg)
 
 if (__name__ == "__main__"):
